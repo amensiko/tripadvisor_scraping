bd8549eafc3e1b0ee452df5a0105cd8d965930df
amensiko <anastasija.mensikova@trincoll.edu>
2019-01-15 11:36:27 -0500

    NN work from 3 weeks ago

--- reviews.py
+++ reviews.py
@@ -66,7 +66,7 @@ def write_item_to_csv(item):
 
 def write_items_to_csv(list_of_items):
 	print(list_of_items)
-	with open('full_photo_reviews_geoloc_dc1.csv', 'a') as res_file:
+	with open('full_photo_reviews_geoloc_dc3.csv', 'a') as res_file:
 		csv_out = csv.writer(res_file)
 		csv_out.writerow(list_of_items)
 
